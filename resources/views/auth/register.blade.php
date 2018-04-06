1bd2e3f807dd3a021c85b0f002e90ea0a79f373d
Jonathan <jonathan.clop@wanadoo.fr>
2018-04-06 11:46:39 +0200

    Finition css inscription, surcharge bootstrap...

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.form')
 <div class="row">
-	<div class="col-md-6">
+    <div class="col-md-6">
 		<div class="colonne_invisible" style="text-align: center;">
 			<img src="../resources/images/logo.svg" width=50><br>
 			<div class="categorie">ForKids ?</div>
@@ -11,75 +11,22 @@
 			Blabla blablabla blalb bla blablalbla blablal bblabl<br><br>
           <a href="{{ route('login') }}" class="btn btn-primary">Je suis déjà inscris</a>
 		</div>
-	</div>
+    </div>
 	<div class="col-md-6">
 		<div class="colonne">
 			<div class="categorie">Inscription</div>
         @slot('title')
             @lang('Inscription')
         @endslot
+        <div class="cadre">
         <form method="POST" action="{{ route('register') }}">
             {{ csrf_field() }}
            {{ csrf_field() }}
             @include('partials.form-group', [
-                'title' => __('NOM Prenom'),
+                'title' => __('NOM Prénom'),
                 'type' => 'text',
                 'name' => 'identity',
                 'required' => true,
-                ])
-                @include('partials.form-group', [
-                'title' => __('NOM Prenom  1er Enfant'),
-                'type' => 'text',
-                'name' => 'kididentity1',
-                'required' => true,
-                ])
-                @include('partials.form-group', [
-                'title' => __('age 1er Enfant'),
-                'type' => 'number',
-                'name' => 'kidage1',
-                'required' => true,
-                ])
-                @include('partials.form-group', [
-                'title' => __('classe 1er Enfant'),
-                'type' => 'text',
-                'name' => 'classkid1',
-                'required' => true,
-                ])
-                @include('partials.form-group', [
-                'title' => __('NOM Prenom 2ème Enfant'),
-                'type' => 'text',
-                'name' => 'kididentity2',
-                'required' => false,
-                ])
-                @include('partials.form-group', [
-                'title' => __('age 2eme Enfant'),
-                'type' => 'number',
-                'name' => 'kidage2',
-                'required' => false,
-                ])
-                 @include('partials.form-group', [
-                'title' => __('classe 2eme Enfant'),
-                'type' => 'text',
-                'name' => 'classkid2',
-                'required' => false,
-                ])
-                @include('partials.form-group', [
-                'title' => __('NOM Prenom 3ème Enfant'),
-                'type' => 'text',
-                'name' => 'kididentity3',
-                'required' => false,
-                ])
-                 @include('partials.form-group', [
-                'title' => __('age 3ème Enfant'),
-                'type' => 'number',
-                'name' => 'kidage3',
-                'required' => false,
-                ])
-                 @include('partials.form-group', [
-                'title' => __('classe 3ème Enfant'),
-                'type' => 'text',
-                'name' => 'classkid3',
-                'required' => false,
                 ])
             @include('partials.form-group', [
                 'title' => __('Adresse email'),
@@ -99,6 +46,91 @@
                 'name' => 'password_confirmation',
                 'required' => true,
                 ])
+                <hr>
+            <table style="width: 100%;">
+            	<tr>
+            	    <td style="width: 50%; padding-right: 5px;">
+            @include('partials.form-group', [
+                'title' => __('NOM Prénom 1er enfant'),
+                'type' => 'text',
+                'name' => 'kididentity1',
+                'required' => true,
+                ])
+                	</td>
+            	    <td style="padding-left: 5px; padding-right: 5px;">
+            @include('partials.form-group', [
+                'title' => __('Âge'),
+                'type' => 'number',
+                'name' => 'kidage1',
+                'required' => true,
+                ])
+                    </td>
+                    <td style="padding-left: 5px;">
+            @include('partials.form-group', [
+                'title' => __('Classe'),
+                'type' => 'text',
+                'name' => 'classkid1',
+                'required' => true,
+                ])
+                    </td>
+                </tr>
+            </table>
+            <table style="width: 100%;">
+            	<tr>
+            	    <td style="width: 50%; padding-right: 5px;">
+            @include('partials.form-group', [
+                'title' => __('NOM Prénom 2ème enfant'),
+                'type' => 'text',
+                'name' => 'kididentity2',
+                'required' => false,
+                ])
+                    </td>
+            	    <td style="padding-left: 5px; padding-right: 5px;">
+            @include('partials.form-group', [
+                'title' => __('Âge'),
+                'type' => 'number',
+                'name' => 'kidage2',
+                'required' => false,
+                ])
+                    </td>
+                    <td style="padding-left: 5px;">
+            @include('partials.form-group', [
+                'title' => __('Classe'),
+                'type' => 'text',
+                'name' => 'classkid2',
+                'required' => false,
+                ])
+                    </td>
+                </tr>
+            </table>
+            <table style="width: 100%;">
+            	<tr>
+            	    <td style="width: 50%; padding-right: 5px;">
+            @include('partials.form-group', [
+                'title' => __('NOM Prénom 3ème enfant'),
+                'type' => 'text',
+                'name' => 'kididentity3',
+                'required' => false,
+                ])
+                    <td style="padding-left: 5px; padding-right: 5px;">
+            @include('partials.form-group', [
+                'title' => __('Âge'),
+                'type' => 'number',
+                'name' => 'kidage3',
+                'required' => false,
+                ])
+                    </td>
+                    <td style="padding-left: 5px;">
+            @include('partials.form-group', [
+                'title' => __('Classe'),
+                'type' => 'text',
+                'name' => 'classkid3',
+                'required' => false,
+                ])
+                    </td>
+                </tr>
+            </table>
+            </div>
             @component('components.button')
                 @lang('Inscription')
             @endcomponent
