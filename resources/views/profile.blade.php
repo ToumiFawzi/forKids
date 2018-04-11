e3d81da3f6601cbc3c1aef2744e1c30513040635
Jonathan <jonathan.clop@wanadoo.fr>
2018-04-11 11:30:26 +0200

    Continuité edition profil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -14,29 +14,45 @@
                         <div class="avatar"></div>
                         <div class="parentsname">
             @include('partials.form-group', [
-                'title' => __('NOM Prénom'),
+                'title' => __(''),
                 'type' => 'text',
                 'name' => 'identity',
                 'required' => true,
                 ])
                         </div>
                     </div>
-                    <table>
-                    <tr>
-                    <td>
-                    <span class="elementlist">Email : </span>
-                    </td>
-                    <td>
-                    @include('partials.form-group', [
-                'title' => __(''),
-                'type' => 'email',
-                'name' => 'email',
-                'required' => true,
-                ])
-                    </td>
-                    </tr>
-                    </table>
-                </div>
+				<table style="width: 100%;">
+					<tr>
+						<td><span class="elementlist">Métier : </span></td>
+						<td style="text-align: right;">@include('partials.form-group', [ 'title' => __(''), 'type' =>
+							'text', 'name' => 'job', 'required' => true, ])</td>
+					</tr>
+					<tr>
+						<td><span class="elementlist">Situation : </span></td>
+						<td style="text-align: right;">@include('partials.form-group', [ 'title' => __(''), 'type' =>
+							'text', 'name' => 'situation', 'required' => true, ])</td>
+					</tr>
+					<tr>
+						<td><span class="elementlist">Téléphone : </span></td>
+						<td style="text-align: right;">@include('partials.form-group', [ 'title' => __(''), 'type' =>
+							'int', 'name' => 'email', 'required' => true, ])</td>
+					</tr>
+					<tr>
+						<td><span class="elementlist">Email : </span></td>
+						<td style="text-align: right;">@include('partials.form-group', [ 'title' => __(''), 'type' =>
+							'email', 'name' => 'email', 'required' => true, ])</td>
+					</tr>
+					<tr>
+						<td><span class="elementlist">Adresse : </span></td>
+						<td style="text-align: right;">@include('partials.form-group', [ 'title' => __(''), 'type' =>
+							'text', 'name' => 'email', 'required' => true, ])</td>
+					</tr>
+				</table>
+            @component('components.button')
+                @lang('Profil')
+            @endcomponent
+				</form>
+				</div>
             </div>
             <div class="col-xl-4">
                 <div class="colonne">
