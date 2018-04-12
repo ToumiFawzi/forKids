1e9dbfd3f6da85594b235c21796639487fc5798f
Jonathan <jonathan.clop@wanadoo.fr>
2018-04-12 10:34:20 +0200

    ajout du bouton supprimer

--- resources/views/components/thematics_href.blade.php
+++ resources/views/components/thematics_href.blade.php
@@ -2,7 +2,7 @@
                         <div class="thematique_lien">
                             <div class="thematique_avatar"></div>
                             <div class="thematique_titre">
-                                <span class="thematique_author">BERNARD Thierry</span> - <span class="thematique_date">Posté le 4 Octobre</span><br>
+                                <span class="thematique_author">BERNARD Thierry</span> - <span class="thematique_date">Posté le 4 Octobre</span><span class="thematique_boutons"><a href="#"><img src="../resources/images/picto_delete.svg" class="picto_small" style="padding-right: -4px;"></a></span><br>
                                 <span class="thematique_content">Quelqu'un pour déposer mon fils à l'école demain matin ?</span><br>
                                 <span class="thematique_last_message">Dernier message aujourd'hui à 18h48</span>
                             </div>
