3d42abe93ed627a25043edf98ad046983ec22283
Jonathan <jonathan.clop@wanadoo.fr>
2018-04-12 09:38:56 +0200

    Ajout input pour le chat avec bouton smiley

--- resources/views/components/foot_input.blade.php
+++ resources/views/components/foot_input.blade.php
@@ -14,5 +14,20 @@
 </script>
 
 <div class="footer">
+    <div class="row">
+        <div class="col-md-3">
     <button class="navbar-toggle" onClick="asideswipe()">+</button> Copyright @ Forkid 2018
+    </div>
+        <div class="col-md-9">
+            <form action="#">
+            <div class="row">
+            <div class="col-md-9">
+            	<input type="text" name="message" id="message">
+            	</div>
+            	<div class="col-md-3">
+            	<input type="submit" value="OK" id="okmessage"> <a href="#"><img src="../resources/images/picto_smileys.svg" class="picto"></a>
+            	</div>
+            	</div>
+            </form>
+        </div>
 </div>
