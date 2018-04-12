001f7a31d2fb4b217a8d0eb7747fbda6c3230170
Jonathan <jonathan.clop@wanadoo.fr>
2018-04-12 10:50:15 +0200

    Mise en place des boutons thematiques

--- resources/views/components/thematics_href.blade.php
+++ resources/views/components/thematics_href.blade.php
@@ -2,7 +2,16 @@
                         <div class="thematique_lien">
                             <div class="thematique_avatar"></div>
                             <div class="thematique_titre">
-                                <span class="thematique_author">BERNARD Thierry</span> - <span class="thematique_date">Posté le 4 Octobre</span><span class="thematique_boutons"><a href="#"><img src="../resources/images/picto_edit.svg" class="picto_small" style="padding-right: -4px;"></a><a href="#"><img src="../resources/images/picto_delete.svg" class="picto_small" style="padding-right: -4px;"></a></span><br>
+                                <table style="width: 100%;">
+                                    <tr>
+                                        <td>
+                                            <span class="thematique_author">BERNARD Thierry</span> - <span class="thematique_date">Posté le 4 Octobre</span>
+                                        </td>
+                                        <td style="text-align: right;">
+                                            <span class="thematique_boutons"><a href="#"><img src="../resources/images/picto_edit.svg" class="picto_small" style="padding-right: -4px;"></a><a href="#"><img src="../resources/images/picto_delete.svg" class="picto_small" style="padding-right: -4px;"></a></span>
+                                        </td>
+                                    </tr>
+                                </table>
                                 <span class="thematique_content">Quelqu'un pour déposer mon fils à l'école demain matin ?</span><br>
                                 <span class="thematique_last_message">Dernier message aujourd'hui à 18h48</span>
                             </div>
